d82a7bdcdb8d74ccede24ac77fd392007a2011a9
katawong0@gmail.com <sotgame123>
2020-10-09 00:25:48 +0700

    update

--- app/src/main/java/com/example/wongnaiassignment/recyclerCrypto/CryptoAdapter.kt
+++ app/src/main/java/com/example/wongnaiassignment/recyclerCrypto/CryptoAdapter.kt
@@ -3,15 +3,20 @@ package com.example.wongnaiassignment.recyclerCrypto
 
 import android.content.Context
 import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.text.Html
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
 import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
@@ -26,15 +31,16 @@ class CryptoAdapter(private val result: ArrayList<CoinsObject>, private val cont
         private val imageCrypto:ImageView = itemView.findViewById(R.id.imageCrypto)
         fun set(position: Int) {
             name.text = result[position].name
-            description.text = result[position].description
+            val des:String? = result[position].description
+            des.let {
+                if(it != null) {
+                    description.text = HtmlCompat.fromHtml(it, HtmlCompat.FROM_HTML_MODE_LEGACY)
+                }else{
+                    description.text = context.getString(R.string.no_description)
+                }
+            }
             val url:String = result[position].iconUrl
-            Glide.with(context).load(url).listener(object : RequestListener<Drawable> {
-                override fun onLoadFailed(
-                    e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
-                    return false }
-                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-                    return false }
-            }).into(imageCrypto)
+            Glide.with(context).load(Uri.parse(url)).into(imageCrypto)
         }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
@@ -52,8 +58,5 @@ class CryptoAdapter(private val result: ArrayList<CoinsObject>, private val cont
 
     override fun getItemCount(): Int {
         return result.size
-    }
-    fun pngLoad(imageCrypto: ImageView, url: String){
-            Glide.with(context).load(url).into(imageCrypto)
     }
 }
