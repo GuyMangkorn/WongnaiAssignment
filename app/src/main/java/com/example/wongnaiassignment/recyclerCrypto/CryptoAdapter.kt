8e9f97d954340b8455729f126a7856591636672f
katawong0@gmail.com <sotgame123>
2020-10-09 15:58:02 +0700

    Set placeholder Glide with Circularprogress

--- app/src/main/java/com/example/wongnaiassignment/recyclerCrypto/CryptoAdapter.kt
+++ app/src/main/java/com/example/wongnaiassignment/recyclerCrypto/CryptoAdapter.kt
@@ -2,14 +2,17 @@ package com.example.wongnaiassignment.recyclerCrypto
 
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.example.wongnaiassignment.R
 
@@ -31,7 +34,7 @@ class CryptoAdapter(private val result: ArrayList<CoinsObject>, private val cont
             url.let {
                 Glide.with(context).load(Uri.parse(it))
                     .thumbnail(0.1f)
-                    .placeholder(R.drawable.ic_outline_cancel_24)
+                    .placeholder(loading())
                     .into(imageCrypto)
             }
         }
@@ -47,5 +50,13 @@ class CryptoAdapter(private val result: ArrayList<CoinsObject>, private val cont
 
     override fun getItemCount(): Int {
         return result.size
+    }
+    private fun loading():Drawable{
+        val circularProgressDrawable = CircularProgressDrawable(context)
+        circularProgressDrawable.strokeWidth = 5f
+        circularProgressDrawable.centerRadius = 30f
+        circularProgressDrawable.setColorSchemeColors(ContextCompat.getColor(context,R.color.colorDescription))
+        circularProgressDrawable.start()
+        return circularProgressDrawable
     }
 }
